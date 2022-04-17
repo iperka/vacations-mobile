568afef979b219eb2c113d4e6ae2fe79077c2def
Michael Beutler <michael.beutler@iperka.com>
2022-04-17 19:21:40 +0200

    Added new `Input` states. 🙌

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,47 +1,101 @@
 import React, {useState} from 'react';
-import {TextInput, TextInputProps} from 'react-native';
+import {
+  ActivityIndicator,
+  StyleProp,
+  TextInput,
+  TextInputProps,
+  TextStyle,
+  View,
+} from 'react-native';
+import Animated, {FadeIn, FadeOut} from 'react-native-reanimated';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {useDeviceContext} from 'twrnc';
 import tw from '../../tailwindcss';
 
-export interface InputProps extends TextInputProps {
-  invalid?: boolean;
-}
-
-const getClassName = (props: {invalid?: boolean; focus?: boolean}): string => {
-  const {invalid, focus} = props;
-  if (invalid) {
-    if (focus) {
-      return 'border-red-500 text-red-500 dark:text-white';
-    }
-    return 'border-red-600 text-red-600 dark:text-white';
-  }
-  if (focus) {
-    return 'border-blue-500 dark:border-blueDark-500 dark:text-white';
-  }
-  return 'border-gray-300 dark:border-gray-500 bg-gray-100 dark:bg-gray-700 dark:text-white';
+const InputIcon: React.FC<{name: string; style: StyleProp<TextStyle>}> = ({
+  name,
+  style,
+}) => {
+  return (
+    <Animated.View
+      entering={FadeIn.duration(350)}
+      exiting={FadeOut.duration(250)}>
+      <Icon style={[tw`text-2xl px-1 font-bold`, style]} name={name} />
+    </Animated.View>
+  );
 };
 
-const Input: React.FC<InputProps> = ({invalid, style, ...props}) => {
+const getClassName = (props: {
+  invalid?: boolean;
+  valid?: boolean;
+  loading?: boolean;
+  focus?: boolean;
+}): string => {
+  if (props.invalid) {
+    return 'bg-red-100 dark:bg-redDark-900 border-red-600 dark:border-redDark-600';
+  }
+  if (props.valid) {
+    return 'bg-green-100 dark:bg-greenDark-900 border-green-600 dark:border-greenDark-600';
+  }
+  if (props.focus) {
+    return 'bg-gray-50 dark:bg-gray-700 border-blue-500 dark:border-blueDark-600';
+  }
+  return 'bg-gray-50 dark:bg-gray-700';
+};
+
+export interface InputProps extends TextInputProps {
+  invalid?: boolean;
+  valid?: boolean;
+  loading?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  invalid,
+  valid,
+  loading,
+  style,
+  ...props
+}) => {
   useDeviceContext(tw);
 
   const [isFocus, setIsFocus] = useState<boolean>(false);
 
   return (
-    <TextInput
-      defaultValue="Test"
+    <View
       style={[
-        tw`my-1 px-4 py-3 rounded-lg border-2`,
-        tw.style(getClassName({invalid, focus: isFocus})),
-        style,
-      ]}
-      onFocus={() => {
-        setIsFocus(true);
-      }}
-      onBlur={() => {
-        setIsFocus(false);
-      }}
-      {...props}
-    />
+        tw`flex flex-row items-center border-2 my-1 rounded-lg border-gray-300 dark:border-gray-500`,
+        tw.style(getClassName({invalid, valid, loading, focus: isFocus})),
+      ]}>
+      <TextInput
+        defaultValue="Test"
+        style={[tw`flex-grow px-4 py-3 dark:text-white`, style]}
+        onFocus={() => {
+          setIsFocus(true);
+        }}
+        onBlur={() => {
+          setIsFocus(false);
+        }}
+        {...props}
+        editable={!loading}
+      />
+
+      {loading ? (
+        <ActivityIndicator style={[tw`pr-2`]} />
+      ) : (
+        invalid && (
+          <InputIcon
+            name="ios-alert-circle"
+            style={[tw`text-red-600 text-redDark-600`]}
+          />
+        )
+      )}
+      {valid && (
+        <InputIcon
+          name="ios-checkmark-circle"
+          style={[tw`text-green-600 text-greenDark-600`]}
+        />
+      )}
+    </View>
   );
 };
 
